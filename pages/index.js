83160dd1d3d1ab1fa421be753aa1508a0187ffbe
Botshelo Tlhabanyane <79863670+BotsheloT@users.noreply.github.com>
2022-11-03 17:29:43 +0200

    Dark Mode

--- pages/index.js
+++ pages/index.js
@@ -33,22 +33,22 @@ export default function Home() {
         <link rel="icon" href="/webfavicon.ico" />
       </Head>
 
-      <main className='bg-white'>
+      <main className='bg-white dark:bg-gray-900'>
         <section className='min-h-screen px-10'>
 
           <nav className='pt-4 mb-3 flex justify-between'>
-            <h1 className='text-xl font-sans font-semibold font'>Botshelo T</h1>
+            <h1 className='text-xl font-sans font-semibold font dark:text-white'>Botshelo T</h1>
             <ul className='flex items-center'>
-              <li> <BsFillMoonFill className='cursor-pointer text-2xl' onClick={() => setDarkMode(!darkMode)}/> </li>
+              <li> <BsFillMoonFill className='cursor-pointer text-2xl dark:text-white' onClick={() => setDarkMode(!darkMode)}/> </li>
               <li> <a href="docs/CV.pdf" className='bg-blue-500 px-3 py-1 rounded-md text-white ml-8 font-medium' download>My CV</a> </li>
             </ul>
           </nav>
 
           <div className='text-center p-10'>
             <h2 className='text-5xl font-medium text-blue-500'>Botshelo Tlhabanyane</h2>
-            <h3 className='font-medium text-2xl'>Software Developer</h3>
+            <h3 className='font-medium text-2xl dark:text-white'>Software Developer</h3>
             <br/>
-            <div className='text-lg text-gray-700'>
+            <div className='text-lg text-gray-700 dark:text-gray-300'>
               <p>Ambitious career-focused Computer Science Student based in Johannesburg.</p>
               <p>Frontend Developement is what interests me the most. I also dabble in Backend Developement.</p>
             </div>
@@ -57,9 +57,9 @@ export default function Home() {
 
           <div className='text-4xl text-gray-500'>
             <ul className='flex justify-center gap-9 hover'>
-              <li className='hover:text-black'> <a href='https://github.com/BotsheloT' target={'_blank'}><AiFillGithub/></a></li>
-              <li className='hover:text-black'> <a href='https://leetcode.com/BotsheloT/' target={'_blank'}><SiLeetcode/></a> </li>
-              <li className='hover:text-black'> <a href='https://www.linkedin.com/in/botshelo-tlhabanyane-36b573210/' target={'_blank'}><AiFillLinkedin/></a></li>              
+              <li className='hover:text-black dark:hover:text-white'> <a href='https://github.com/BotsheloT' target={'_blank'}><AiFillGithub/></a></li>
+              <li className='hover:text-black dark:hover:text-white'> <a href='https://leetcode.com/BotsheloT/' target={'_blank'}><SiLeetcode/></a> </li>
+              <li className='hover:text-black dark:hover:text-white'> <a href='https://www.linkedin.com/in/botshelo-tlhabanyane-36b573210/' target={'_blank'}><AiFillLinkedin/></a></li>              
             </ul>
           </div>
 
@@ -68,8 +68,8 @@ export default function Home() {
           </div>
 
           <div className='py-10 text-center'>
-            <h3 className='font-medium text-2xl pb-3'>About Me</h3>
-            <div className='text-lg text-gray-700'>
+            <h3 className='font-medium text-2xl pb-3 dark:text-white'>About Me</h3>
+            <div className='text-lg text-gray-700 dark:text-gray-300'>
               <p>Design and my sense of aesthetics are reflected in all areas of my daily life.</p>
               <p>I personally believe that everyone has a unique characteristic to them and that it can only be</p>
               <p></p>
@@ -84,9 +84,9 @@ export default function Home() {
           </div>
 
           <div className='py-10 text-center'>
-          <h3 className=' font-medium text-2xl pb-3'>Why SovTech?</h3>
+          <h3 className=' font-medium text-2xl pb-3 dark:text-white'>Why SovTech?</h3>
 
-          <div className='text-lg text-gray-700'>
+          <div className='text-lg text-gray-700 dark:text-gray-300'>
             <p>Ever since I was introduced to SovTech, my interest for the company has grown.
             Through SovTech's Social Media, I have witnessed how seriously they take technology and its potential.            
             I truly believe that with working at SovTech, I will be surrounded by like-minded developers who will allow me to advance in the IT fraternity of SovTech.</p>            
@@ -103,12 +103,12 @@ export default function Home() {
       <section className='px-10'>
 
         <div className='text-center py-10'>
-          <h3 className=' font-medium text-2xl'>Skills</h3>
+          <h3 className=' font-medium text-2xl dark:text-white'>Skills</h3>
 
           <div id="accordion-collapse" data-accordion="open">
 
             <h2 id="accordion-collapse-heading-1">
-            <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 hover:bg-gray-100 bg-gray-100 text-black text-lg" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
+            <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 hover:bg-gray-100 bg-gray-100 text-black text-lg dark:bg-gray-700 dark:border-gray-500 dark:text-white" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
               <span>Frontend</span>
               <svg data-accordion-icon="" class="w-6 h-6 shrink-0 text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </button>
@@ -129,7 +129,7 @@ export default function Home() {
             </div>
 
             <h2 id="accordion-collapse-heading" data-accordion="open" >
-            <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 hover:bg-gray-100 bg-gray-100 text-black text-lg" data-accordion-target="#accordion-collapse-body-2" aria-expanded="false" aria-controls="accordion-collapse-body-2">
+            <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 hover:bg-gray-100 bg-gray-100 text-black text-lg dark:bg-gray-700 dark:border-gray-500 dark:text-white" data-accordion-target="#accordion-collapse-body-2" aria-expanded="false" aria-controls="accordion-collapse-body-2">
             <span>Backend</span>
             <svg data-accordion-icon="" class="w-6 h-6 shrink-0 text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </button>
@@ -147,7 +147,7 @@ export default function Home() {
             </div>
 
             <h2 id="accordion-collapse-heading-3">
-            <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left border border-b-0 border-gray-200 rounded-b-xl focus:ring-4 focus:ring-gray-200 hover:bg-gray-100 bg-gray-100 text-black text-lg" data-accordion-target="#accordion-collapse-body-3" aria-expanded="false" aria-controls="accordion-collapse-body-3">
+            <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left border border-b-0 border-gray-200 rounded-b-xl focus:ring-4 focus:ring-gray-200 hover:bg-gray-100 bg-gray-100 text-black text-lg dark:bg-gray-700 dark:border-gray-500 dark:text-white" data-accordion-target="#accordion-collapse-body-3" aria-expanded="false" aria-controls="accordion-collapse-body-3">
             <span>Design</span>
             <svg data-accordion-icon="" class="w-6 h-6 shrink-0 text-blue-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </button>
@@ -164,19 +164,19 @@ export default function Home() {
 
         </div>
         
-          <div className='w-auto h-auto bg-gray-100 rounded-lg bg-center text-center my-10'>
+          <div className='w-auto h-auto bg-gray-100 rounded-lg bg-center text-center my-10 dark:bg-gray-600'>
 
-            <h3 className='font-medium text-2xl pb-4 py-5'>Contact Me</h3>
+            <h3 className='font-medium text-2xl pb-4 py-5 dark:text-white'>Contact Me</h3>
 
             <div>
               <div className='flex flex-col justify-center items-center py-5'>
             
                 <form ref={form} onSubmit={sendEmail}>
-                <input type={'text'} placeholder = 'Name' name='user_name' className='bg-white w-96 h-9 rounded-md mb-5 placeholder: pl-2' required/>
+                <input type={'text'} placeholder = 'Name' name='user_name' className='bg-white w-96 h-9 rounded-md mb-5 placeholder: pl-2 dark:bg-gray-400 dark:placeholder:text-gray-100' required/>
                 <br/>
-                <input type={'email'} placeholder = 'Email Address' name='user_email' className='bg-white w-96 h-9 rounded-md mb-5 placeholder: pl-2' required/>
+                <input type={'email'} placeholder = 'Email Address' name='user_email' className='bg-white w-96 h-9 rounded-md mb-5 placeholder: pl-2 dark:bg-gray-400 dark:placeholder:text-gray-100' required/>
                 <br/>
-                <textarea type={'text'} placeholder = 'Message' name='message' className='bg-white w-96 h-56 rounded-md mb-5 placeholder: pl-2 pt-1' required/>
+                <textarea type={'text'} placeholder = 'Message' name='message' className='bg-white w-96 h-56 rounded-md mb-5 placeholder: pl-2 pt-1 dark:bg-gray-400 dark:placeholder:text-gray-100' required/>
                 <br/>
                 <button type="submit"  value={"Send"}  className='bg-blue-500 px-3 py-1 rounded-md text-white text-lg font-medium w-96 h-9'> Send Message </button>
                 </form>
